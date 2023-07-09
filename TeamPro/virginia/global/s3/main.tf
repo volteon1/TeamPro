ed251fdc9f4143a0f540b315a320256f8a482531
volteon1 <136070380+volteon1@users.noreply.github.com>
2023-07-09 14:26:39 +0900

    Add files via upload

---
+++ TeamPro/virginia/global/s3/main.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "us-east-1"
+
+  # 2.x 버전의 AWS 공급자 허용
+  version = "~> 2.0"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+
+  bucket = var.bucket_name
+
+  // This is only here so we can destroy the bucket as part of automated tests. You should not copy this for production
+  // usage
+  force_destroy = true
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+
+  # 서버사이드 암호화 설정
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = var.table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
---
+++ TeamPro/virginia/stage/services/webserver-cluster/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region = "us-east-1"
+
+  # 2.x 버전의 AWS 공급자 허용
+  version = "~> 2.0"
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+
+  cluster_name           = var.cluster_name
+  db_remote_state_bucket = var.db_remote_state_bucket
+  db_remote_state_key    = var.db_remote_state_key
+
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 3
+}
+
+resource "aws_security_group_rule" "allow_testing_inbound" {
+  type              = "ingress"
+  security_group_id = module.webserver_cluster.alb_security_group_id
+
+  from_port   = 12345
+  to_port     = 12345
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
